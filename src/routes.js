06433cbe647bbdf8ed569fbe9e369167a03ff0d4
Zakhar Gulchak <z.gulchak@levi9.com>
2016-07-27 18:17:41 +0300

    Create TextInput component Added saving data Added redirection + notification after saving Added validation for form Uses propTypes, willTransitionFrom Added autofilling for author/:id form

--- src/routes.js
+++ src/routes.js
@@ -7,11 +7,13 @@ var DefaultRoute = Router.DefaultRoute;
 var Route = Router.Route;
 var NotFoundRoute = Router.NotFoundRoute;
 var Redirect = Router.Redirect;
-
+ // todo : make 404 page works
 var routes = (
 	<Route name="app" path="/" handler={require('./components/app')}>
 		<DefaultRoute handler={require('./components/homePage')} />
 		<Route name="authors" handler={require('./components/authors/authorPage')} />
+		<Route name="addAuthor" path="author" handler={require('./components/authors/manageAuthorPage')} />
+		<Route name="manageAuthor" path="author/:id" handler={require('./components/authors/manageAuthorPage')} />
 		<Route name="about" handler={require('./components/about/aboutPage')} />
 		<NotFoundRoute handler={require('./components/notFoundPage')} />
 		<Redirect from="about-us" to="about" />
